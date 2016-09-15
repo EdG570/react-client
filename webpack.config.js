c9d055041dc09824a8c0091b98006d02249ec3be
Joe Tagliaferro <joe@cinematique.com>
2016-09-14 20:19:57 -0400

    Webpack setup and dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlTemplate = require('html-webpack-template');
+
+module.exports = {
+    entry: [
+        './index.js',
+        'webpack/hot/dev-server'
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot!babel'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css!postcss'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: 'dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true,
+        hot: true
+    },
+    devtool: 'source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'ERP Project',
+            inject: false,
+            template: HtmlTemplate,
+            appMountId: 'app',
+            mobile: true,
+            minify: {
+                collapseWhitespace: true
+            }
+        })
+    ],
+    postcss: function () {
+        return [autoprefixer];
+    }
+};

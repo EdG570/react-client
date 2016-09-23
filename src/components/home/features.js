4dad18b564fce501c60c2362b0476d78631a2581
EdG570 <cole570@hotmail.com>
2016-09-22 20:06:39 -0400

    Moved all home page partials in their own folder

--- src/components/home/features.js
+++ src/components/home/features.js
@@ -6,22 +6,22 @@ export default () => {
       <div>
         <img src="src/assets/organize.svg" alt="Organize icon"/>
         <h2>Organize</h2>
-        <p>Organizing future tasks couldn't be easier. Utilize lists to
+        <p>Organizing tasks couldn't be easier. Utilize lists to
         store and organize your tasks. Simplexity uses a color coded
-        priority system to help you visually prioritize tasks.</p>
+        priority system to help you prioritize your tasks.</p>
       </div>
       <div>
         <img src="src/assets/collaborate.svg" alt="Collaborate icon"/>
         <h2>Collaborate</h2>
         <p>Collaborate with friends, family and coworkers. Share a project
-        and discuss your game plan for tackling the tasks.</p>
+        and discuss your game plan for tackling the project.</p>
       </div>
       <div>
         <img src="src/assets/weather.svg" alt="Weather icon"/>
-        <h2>Weather</h2>
+        <h2>Plan</h2>
         <p>Not all tasks can be completed if the weather won't cooperate.
-        Simplexity will provide your current local weather so planning
-        which tasks are feasible just became even more efficient.</p>
+        Simplexity provides your current local weather so planning
+        which tasks are feasible just became even easier.</p>
       </div>
     </div>
   );

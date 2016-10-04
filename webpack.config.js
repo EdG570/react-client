150d5a90d82b96d15b5a9b968f570804979f156d
Joe Tagliaferro <joe@cinematique.com>
2016-10-03 21:49:05 -0400

    Fixes SASS loader and adds features.scss

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            {test: /\.scss$/i, loaders: ['style', 'css', 'sass']}
         ]
     },
     plugins: [
